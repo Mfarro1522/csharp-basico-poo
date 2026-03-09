bfa9e0ea4d4c74b9d8684ba6a3cc6859bbd20948
Mfarro1522 <mauriciofarroizquierdo@gmail.com>
2026-03-10 00:51:21 +0100

    Primer Proyecto

---
+++ Basico/Primeros pasos/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿//Console.WriteLine("aea y que pasa si hago esto lololollollo tonotos");
+
+//programa que calcula el area de un rectangulo Area = base * altura 
+var LadoA = 1.1;
+var LadoB = 1.1;
+var Resultado = 1.1;
+
+Console.WriteLine("calcula el area de un rectangulo");
+Console.WriteLine("ingrese el valor del Lado A");
+LadoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("ingrese el valor del Lado B");
+LadoB = Convert.ToDouble(Console.ReadLine());
+
+Resultado = LadoA * LadoB;
+Console.WriteLine("El Resultado es : "+Resultado);
